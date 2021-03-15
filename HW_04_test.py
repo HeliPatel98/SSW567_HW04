819ee9c389dfec46c02e23074f0c66c9a21b4b83
HeliPatel98 <patelheli2598@gmail.com>
2021-03-15 14:26:30 -0400

    HW_04

--- HW_04_test.py
+++ HW_04_test.py
@@ -2,13 +2,12 @@ import unittest
 from HW_04 import get_repo
 
 class TestgetRepo(unittest.TestCase):
-
     def test_repo(self):
         expected = ['User: HeliPatel98',
                     'Repository: helloworld Number of commits: 2',
                     'Repository: SSW-567 Number of commits: 2',
                     'Repository: SSW-695_COOKIT Number of commits: 1',
-                    'Repository: SSW567_HW04 Number of commits: 10',
+                    'Repository: SSW567_HW04 Number of commits: 8',
                     'Repository: Student_Repository Number of commits: 23',
                     'Repository: Triangle567 Number of commits: 17']
 
